525984ce9bdd5495cf8a6cbc90e3d5f858f1a7a9
raouane <r.difi@esi-sba.dz>
2026-06-01 21:59:04 +0100

    fix case app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './app.jsx'
 import axios from 'axios' 
 import './i18n.js'
 axios.defaults.baseURL = 'https://reviving-throwback-cringing.ngrok-free.dev';
